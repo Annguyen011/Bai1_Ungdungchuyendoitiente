765855b2ae14cb0782762e8ef4e7cb2a0f192133
Anngu <Anngu@MSI>
2023-03-30 23:55:28 +0700

    Add project files.

---
+++ Bai1_Ungdungchuyendoitiente/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_Ungdungchuyendoitiente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vnd, usd;
+            const int tiGia = 23000;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine($"Tỉ giá hiện tại giữ VND và USD là 1 USD = {tiGia} VND");
+            Console.Write("Mời nhập giá tiền USD = ");
+            usd = double.Parse(Console.ReadLine());
+            vnd = usd * tiGia;
+            Console.WriteLine($"Giá VND là : {vnd}");
+            Console.ReadLine();
+
+        }
+    }
+}
